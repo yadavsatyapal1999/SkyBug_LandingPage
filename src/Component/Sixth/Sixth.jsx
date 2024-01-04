b685ede673ec6d7d81986363a6d414f5df2f6540
yadavsatyapal1999 <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-01-04 23:01:16 +0530

    Sixth page css done

--- src/Component/Sixth/Sixth.jsx
+++ src/Component/Sixth/Sixth.jsx
@@ -7,50 +7,50 @@ export default function Sixth() {
 
     return <div id="sixth">
 
-        <div>
-            <p>CONTACT US</p>
+        <div id="center" >
+            <p className="green">CONTACT US</p>
             <h1>
                 Reach out for a new project or just say hello.
             </h1>
         </div>
-<div  className="form" >
-<div className="message">
-            <p>SEND US MESSAGE</p>
-            <input type="text" placeholder="Your Name" /> <br />
-            <input type="email" placeholder="Your Email" /> <br />
-            <input type="text" placeholder="Subject" /> <br />
-            <textarea name="Message" id="" cols="30" rows="10">
-                Message
-            </textarea>  <br />
-            <button> SUBMIT </button>
-        </div>
-        <div>
-            <p>CONTACT INFO</p>
-            <div>
-                <p>WHERE TO FIND US</p>
-                <p>1600 Amphitheatre Parkway
-                    Mountain View, CA
-                    94043 US</p>
+        <div className="form" >
+            <div className="message">
+                <p>SEND US MESSAGE</p>
+                <input type="text" placeholder="Your Name" /> <br />
+                <input type="email" placeholder="Your Email" /> <br />
+                <input type="text" placeholder="Subject" /> <br />
+                <textarea name="Message" id="" cols="30" rows="10">
+                    Message
+                </textarea>  <br />
+                <button> SUBMIT </button>
             </div>
             <div>
-                <p>Email us At</p>
-                <p>
-                    contact@glintsite.com
-                    <br />
-                    info@glintsite.com
-                </p>
-            </div>
-            <div>
-                <p>Call Us At</p>
-                <p>
-                    Phone: (+63) 555 1212 <br />
-                    Mobile: (+63) 555 0100 <br />
-                    Fax: (+63) 555 0101
-                </p>
-            </div>
+                <p className="green" >CONTACT INFO</p>
+                <div>
+                    <p>WHERE TO FIND US</p>
+                    <p>1600 Amphitheatre Parkway
+                        Mountain View, CA
+                        94043 US</p>
+                </div>
+                <div>
+                    <p className="green" >Email us At</p>
+                    <p>
+                        contact@glintsite.com
+                        <br />
+                        info@glintsite.com
+                    </p>
+                </div>
+                <div>
+                    <p className="green" >Call Us At</p>
+                    <p>
+                        Phone: (+63) 555 1212 <br />
+                        Mobile: (+63) 555 0100 <br />
+                        Fax: (+63) 555 0101
+                    </p>
+                </div>
 
+            </div>
         </div>
-</div>
     </div>
 
 }
